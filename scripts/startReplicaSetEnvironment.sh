cf2f9b183e8a20540762f1d2e0a73957f8379782
whistleyz <whistleyz@163.com>
2023-04-04 11:30:20 +0800

    fix: docker compose sh

--- scripts/startReplicaSetEnvironment.sh
+++ scripts/startReplicaSetEnvironment.sh
@@ -3,9 +3,6 @@
 DELAY=10
 
 docker compose --file docker-compose-replicaset.yml down
-docker rm -f $(docker ps -a -q)
-docker volume rm $(docker volume ls -q)
-
 docker compose --file docker-compose-replicaset.yml up -d
 
 echo "****** Waiting for ${DELAY} seconds for containers to go up ******"
