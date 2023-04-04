34298cff1af6c53dbeeab4a958b1e2dcc73da1f4
whistleyz <whistleyz@163.com>
2023-04-04 11:21:28 +0800

    fix: docker compose sh

--- scripts/startReplicaSetEnvironment.sh
+++ scripts/startReplicaSetEnvironment.sh
@@ -11,4 +11,4 @@ docker compose --file docker-compose-replicaset.yml up -d
 echo "****** Waiting for ${DELAY} seconds for containers to go up ******"
 sleep $DELAY
 
-docker exec mongo-rs /scripts/rs-init.sh
+docker exec mongo-rs sh /scripts/rs-init.sh
